72406acf0cd66278952dcf8d15e331762590494c
ekamleen <ekamleenm@gmail.com>
2024-02-14 13:42:30 -0800

    Idk how many files added

---
+++ MavenJava/src/main/java/PackageA/Greetings.java
@@ -0,0 +1,9 @@
+package PackageA;
+
+import static PackageB.Message.message;
+
+public class Greetings {
+    public static void main(String[] args) {
+        message();
+    }
+}
---
+++ MavenJava/src/main/java/PackageB/Message.java
@@ -0,0 +1,7 @@
+package PackageB;
+
+public class Message {
+   public static void message(){
+       System.out.println("this is message from message class!!");
+   }
+}
