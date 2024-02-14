9564585eefec2f4baa3494c7c6f788709d31f367
ekamleen <ekamleenm@gmail.com>
2024-02-13 22:48:02 -0800

    Demo_2.java updated

--- OOP-KK/OOP_1/Demo_2.java
+++ OOP-KK/OOP_1/Demo_2.java
@@ -1,2 +1,10 @@
 package JavaOOP;
-
+// Wrapper class
+public class Demo_2{
+    public static void main(String[] args) {
+        int a = 10;    // Primitive data types are in stack so there is no pass by reference, therefore copies get passed
+        Integer num = 10;  // Object: are in heap their reference value is passed i.e. num is copied and passed and num
+                           // nnum is refernce var, Here Integer is FINAL so that why it wont be swapped but in general
+                          // have thier address passed unlike primitive data types.
+    }
+}
