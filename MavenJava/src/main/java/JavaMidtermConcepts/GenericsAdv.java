8862bf82fb0e093d381fe22e65d6112086ce4ab7
ekamleen <ekamleenm@gmail.com>
2024-03-04 23:40:38 -0800

    bit more on generics

---
+++ MavenJava/src/main/java/JavaMidtermConcepts/GenericsAdv.java
@@ -0,0 +1,16 @@
+package JavaMidtermConcepts;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class GenericsAdv <T,V> {
+    T thingToSay;
+    V personToSay;
+
+    public GenericsAdv(T thingToSay,V personToSay){
+       this.personToSay = personToSay;
+       this.thingToSay = thingToSay;
+    }
+    public void LetsDoIt(){
+        System.out.println("Hey " + this.personToSay + "I just wanted to tell you that " + this.thingToSay + ".");
+    }
+}
---
+++ MavenJava/src/main/java/JavaMidtermConcepts/LetsSayIt.java
@@ -0,0 +1,8 @@
+package JavaMidtermConcepts;
+
+public class LetsSayIt {
+    public static void main(String[] args) {
+        GenericsAdv<String, String> sayBeforeItsTooLate = new GenericsAdv<>("I care about You ","Shaina ");
+        sayBeforeItsTooLate.LetsDoIt();
+    }
+}
